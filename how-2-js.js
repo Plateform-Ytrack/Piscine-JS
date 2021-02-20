241e5547550c386a71f17f14026e25bd86b66832
Amol44 <39632426+Amol44@users.noreply.github.com>
2021-02-20 21:08:38 +0100

    First

---
+++ how-2-js.js
@@ -0,0 +1,12 @@
+function name(Prénom){
+    let retour = "Hello; ";
+    retour = retour + Prénom;
+    return retour;
+}
+
+console.log(name(Malo));
+
+const str = 'abc'
+const num = 10
+const bool = true
+const undef = undefined
